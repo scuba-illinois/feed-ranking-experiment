de0eec476ef25f05eb4e8b6e9ed5b29a2dfe8840
Jackie Chan <jackiec1998@gmail.com>
2025-06-04 13:32:57 -0500

    Have the ability to flag posts and a timer.

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -21,20 +21,78 @@ type FeedData = {
 	y: number;
 }[];
 
-function FeedButtons({ feedData }: { feedData: FeedData }) {
-	return feedData.map(({ y, height, uuid }) => (
-		<button
-			key={uuid}
-			className="py-2 px-3 shadow-lg rounded-md text-[10pt] bg-blue-500 text-white hover:bg-blue-600 transition-colors"
-			style={{
-				position: "absolute",
-				left: "475px",
-				top: `${height / 2 + y - 20}px`,
-			}}
-		>
-			Flag
-		</button>
-	));
+function FeedButtons({
+	feedData,
+	selectedPosts,
+	setSelectedPosts,
+}: {
+	feedData: FeedData;
+	selectedPosts: string[];
+	setSelectedPosts: React.Dispatch<React.SetStateAction<string[]>>;
+}) {
+	function FlagButton({
+		uuid,
+		height,
+		y,
+	}: {
+		uuid: string;
+		height: number;
+		y: number;
+	}) {
+		return (
+			<button
+				key={uuid}
+				className="py-2 px-3 shadow-lg rounded-md text-[10pt] bg-blue-500 text-white hover:bg-blue-600 transition-colors"
+				style={{
+					position: "absolute",
+					left: "475px",
+					top: `${height / 2 + y - 20}px`,
+				}}
+				onClick={() => {
+					if (selectedPosts.length < 3) {
+						setSelectedPosts((state) => [...state, uuid]);
+					}
+				}}
+			>
+				Flag
+			</button>
+		);
+	}
+
+	function UnflagButton({
+		uuid,
+		height,
+		y,
+	}: {
+		uuid: string;
+		height: number;
+		y: number;
+	}) {
+		return (
+			<button
+				key={uuid}
+				className="py-2 px-3 shadow-lg rounded-md text-[10pt] bg-orange-500 text-white hover:bg-orange-600 transition-colors"
+				style={{
+					position: "absolute",
+					left: "470px",
+					top: `${height / 2 + y - 20}px`,
+				}}
+				onClick={() =>
+					setSelectedPosts((state) => state.filter((_uuid) => _uuid !== uuid))
+				}
+			>
+				Unflag
+			</button>
+		);
+	}
+
+	return feedData.map(({ y, height, uuid }) => {
+		if (!selectedPosts.includes(uuid)) {
+			return <FlagButton uuid={uuid} height={height} y={y} />;
+		} else {
+			return <UnflagButton uuid={uuid} height={height} y={y} />;
+		}
+	});
 }
 
 function FeedView({ fileName, height }: { fileName: string; height: number }) {
@@ -51,10 +109,12 @@ function FeedView({ fileName, height }: { fileName: string; height: number }) {
 function Feed() {
 	// TODO: Randomize this, right now you're just using one rotation/snapshot.
 	const FEED_IMAGE = `${snapshots[0]}/rotation-${0}.png`;
+	const TIMER_SETTING = 1200000;
 
 	const [feedData, setFeedData] = useState<FeedData | null>(null);
 	const [isVisible, setIsVisible] = useState(false);
 	const [timeLeft, setTimeLeft] = useState(0);
+	const [selectedPosts, setSelectedPosts] = useState<string[]>([]);
 
 	// Retrieve the JSON describing the image.
 	useEffect(() => {
@@ -70,6 +130,7 @@ function Feed() {
 		);
 	}, []);
 
+	// Handling timer.
 	useEffect(() => {
 		if (!isVisible || timeLeft <= 0) {
 			return;
@@ -96,23 +157,49 @@ function Feed() {
 	} else {
 		return (
 			<div className="flex justify-center h-[100vh] gap-2 p-4">
-				<div className="flex flex-col w-[560px]">
-					<div className="m-3">
-						<Header>Trending on Reddit</Header>
-						<Body>To start assessing posts, please click on any post.</Body>
-						{isVisible && <Body>{formatTime(timeLeft)}</Body>}
+				<div className="flex flex-col w-[530px]">
+					<div className="flex flex-col gap-2 mb-4">
+						<Header>Directions</Header>
+						<Body>
+							Here, you will be shown a screenshot of Reddit's r/popular feed
+							containing 10 posts. You will have 2 minutes to browse and select
+							at most 3 posts from this feed that you would like to read more
+							about.
+						</Body>
+						<Body>
+							Next to each post on the screenshot of the feed, there is a "Flag"
+							button to denote that you would like to read more about this post.
+						</Body>
+						<Body>
+							To start, press the "Show Feed" button. The timer and feed will
+							appear below once to press the button.
+						</Body>
+						{isVisible && (
+							<div className="flex justify-between items-center w-full">
+								<Body>
+									<b className="text-black">Time Remaining: </b>
+									{formatTime(timeLeft)}
+								</Body>
+								<Body>
+									<span>
+										<b className="text-black">Posts Selected:</b>{" "}
+										{selectedPosts.length.toLocaleString()}
+									</span>
+								</Body>
+							</div>
+						)}
+						{!isVisible && (
+							<button
+								className="py-2 px-3 shadow-lg rounded-md text-[10pt] bg-blue-500 text-white hover:bg-blue-600 transition-colors"
+								onClick={() => {
+									setIsVisible(true);
+									setTimeLeft(TIMER_SETTING);
+								}}
+							>
+								Show Feed
+							</button>
+						)}
 					</div>
-					{!isVisible && (
-						<button
-							className="py-2 px-3 shadow-lg rounded-md text-[10pt] bg-blue-500 text-white hover:bg-blue-600 transition-colors mb-4"
-							onClick={() => {
-								setIsVisible(true);
-								setTimeLeft(10); // Reset to 10 seconds
-							}}
-						>
-							Show Feed
-						</button>
-					)}
 
 					{isVisible && (
 						<div
@@ -124,7 +211,11 @@ function Feed() {
 								// Use the last post's y and height to set the image height.
 								height={feedData[9].y + feedData[9].height}
 							/>
-							<FeedButtons feedData={feedData} />
+							<FeedButtons
+								feedData={feedData}
+								selectedPosts={selectedPosts}
+								setSelectedPosts={setSelectedPosts}
+							/>
 						</div>
 					)}
 				</div>
