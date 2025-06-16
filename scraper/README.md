744bc4045a40004341d281d99440fc1057b4feaf
Jackie Chan <jackiec1998@gmail.com>
2025-06-16 18:18:00 -0500

    Minor edits to the scraping script so that files are named appropriately.

--- scraper/README.md
+++ scraper/README.md
@@ -3,8 +3,3 @@
 The `index.js` file launches a web browser and navigates to the r/popular feed on Reddit. On the feed, it screenshots the first 10 posts on the feed as one long PNG file. It also records the position/height of each post to position the buttons on the UI correctly. Without the coordinates for each post on the screenshot, the UI/frontend wouldn't know where to place the buttons because it's just one long screenshot.
 
 To run, use `node index.js`.
-
-## TODO
-
-- [ ] Change the names of the files to be shorter.
-- [ ] Check if the `y` + `height` of the last post is at the correct position, i.e., flushed at the end of the last post.
--- scraper/index.js
+++ scraper/index.js
@@ -1,15 +1,20 @@
 import puppeteer from "puppeteer";
 import fs from "node:fs/promises";
 import path from "node:path";
+import { v4 as uuid } from "uuid";
 
-const date = new Date().toISOString().split(".")[0] + "Z";
+const snapshotID = String(uuid());
+let ids = Array.from({ length: 10 }, () => uuid());
+const originalOrdering = [...ids];
 
-const outDir = `./data/${date}/`;
+// Create the directory.
+const outDir = `./data/${snapshotID}/`;
 await fs.mkdir(outDir, { recursive: true });
 
 const browser = await puppeteer.launch({ headless: false });
 const page = await browser.newPage();
 
+// Use light mode.
 await page.emulateMediaFeatures([
 	{ name: "prefers-color-scheme", value: "light" },
 ]);
@@ -17,10 +22,10 @@ await page.emulateMediaFeatures([
 await page.goto("https://www.reddit.com/r/popular/");
 await page.setViewport({ width: 500, height: 9999, deviceScaleFactor: 2 });
 
-// Wait until 10 articles have been loaded
+// Wait until 10 articles have been loaded.
 await page.waitForSelector("shreddit-feed > :nth-child(10 of article)");
 
-// Remove ads
+// Remove ads.
 await page.evaluate(() => {
 	document
 		.querySelectorAll("shreddit-feed > shreddit-ad-post")
@@ -30,35 +35,38 @@ await page.evaluate(() => {
 		});
 });
 
-// Screenshot of individual articles
+// Screenshot of individual articles/
 for (let i = 1; i < 11; i++) {
 	const article = await page.waitForSelector(
 		`shreddit-feed > :nth-child(${i} of article)`
 	);
 
+	// Using the IDs, name the individual posts appropriately.
 	await article.screenshot({
-		path: path.join(outDir, `article-${i}.png`),
+		path: path.join(outDir, `${ids[i - 1]}.png`),
 	});
 }
 
-// Screenshots of rotations
+// Screenshots of rotations/
 for (let i = 0; i < 10; i++) {
-	// Get BB of each article
+	// Get bounds of each article.
 	const articleBounds = [];
 	for (let j = 1; j < 11; j++) {
 		const article = await page.waitForSelector(
 			`shreddit-feed > :nth-child(${j} of article)`
 		);
-		articleBounds.push(await article.boundingBox());
+		articleBounds.push({ ...(await article.boundingBox()), id: ids[j - 1] });
 	}
+
+	// Save the bounds of the articles.
 	await fs.writeFile(
-		path.join(outDir, `article-bounds-rot-${i}.json`),
+		path.join(outDir, `rotation-${i}.json`),
 		JSON.stringify(articleBounds)
 	);
 
 	// Take screenshot
 	await page.screenshot({
-		path: path.join(outDir, `full-page-rot-${i}.png`),
+		path: path.join(outDir, `rotation-${i}.png`),
 	});
 
 	// Rotate 10th post into 1st rank
@@ -74,6 +82,20 @@ for (let i = 0; i < 10; i++) {
 		parent.prepend(hr);
 		parent.prepend(article);
 	});
+
+	// Move the last ID to the front of the array.
+	ids = [ids.pop(), ...ids];
 }
+
+await fs.writeFile(
+	path.join(outDir, "meta.json"),
+	JSON.stringify({
+		timestamp: new Date().toISOString(),
+		snapshotID: snapshotID,
+		originalOrdering: Object.fromEntries(
+			originalOrdering.map((id, idx) => [idx + 1, id])
+		),
+	})
+);
 
 await browser.close();
