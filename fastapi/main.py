d5c5ee92c1c282b7f98d004e743343cc29785509
Jackie Chan <jackiec1998@gmail.com>
2025-06-25 17:54:41 -0500

    Backend serves the 'no proof' snapshot first.

--- fastapi/main.py
+++ fastapi/main.py
@@ -63,7 +63,11 @@ async def validate_participant(participant_id: str):
     """
     TODO: Eventually deprecate this when all snapshots are taken using JPGs.
     """
-    JPG_SNAPSHOTS = {"38dbd51f-fbff-4541-813b-80678f03ba43"}
+    JPG_SNAPSHOTS = {
+        "38dbd51f-fbff-4541-813b-80678f03ba43",
+        "5e24e85b-40db-4fa8-ae6e-cfe16cd6920e",
+    }
+    NO_PROOF_SNAPSHOTS = {"5e24e85b-40db-4fa8-ae6e-cfe16cd6920e"}
 
     # Load the CSV file into a DataFrame
     df = pd.read_csv("IDs.csv")
@@ -76,9 +80,10 @@ async def validate_participant(participant_id: str):
 
     # Read the meta.json for each feed UUID.
     feeds = [
+        "5e24e85b-40db-4fa8-ae6e-cfe16cd6920e",  # No proof snapshot
         "38dbd51f-fbff-4541-813b-80678f03ba43",  # New JPG snapshot
         # "20e1d2ef-2268-4d4b-a724-5f5d61cfc896",
-        "36a937d7-4fc4-4cbf-8705-83776c112078",
+        # "36a937d7-4fc4-4cbf-8705-83776c112078",
         "5c18c574-32db-4028-b4ea-40e949ff81ba",
     ]
 
@@ -96,21 +101,36 @@ async def validate_participant(participant_id: str):
             # get_feed("public/20e1d2ef-2268-4d4b-a724-5f5d61cfc896/rotation-0.png"),
             get_feed(
                 f"public/{feeds[0]}/rotation-{rotations[0]}"
+                + ("-no-proof" if feeds[0] in NO_PROOF_SNAPSHOTS else "")
                 + (".jpg" if feeds[0] in JPG_SNAPSHOTS else ".png")
             ),
             get_feed(
                 f"public/{feeds[1]}/rotation-{rotations[1]}"
+                + ("-no-proof" if feeds[1] in NO_PROOF_SNAPSHOTS else "")
                 + (".jpg" if feeds[1] in JPG_SNAPSHOTS else ".png")
             ),
             get_feed(
                 f"public/{feeds[2]}/rotation-{rotations[2]}"
+                + ("-no-proof" if feeds[2] in NO_PROOF_SNAPSHOTS else "")
                 + (".jpg" if feeds[2] in JPG_SNAPSHOTS else ".png")
             ),
         ],
         "feedData": {
-            feeds[0]: get_feed_data(f"public/{feeds[0]}/rotation-{rotations[0]}.json"),
-            feeds[1]: get_feed_data(f"public/{feeds[1]}/rotation-{rotations[1]}.json"),
-            feeds[2]: get_feed_data(f"public/{feeds[2]}/rotation-{rotations[2]}.json"),
+            feeds[0]: get_feed_data(
+                f"public/{feeds[0]}/rotation-{rotations[0]}"
+                + ("-no-proof" if feeds[0] in NO_PROOF_SNAPSHOTS else "")
+                + ".json"
+            ),
+            feeds[1]: get_feed_data(
+                f"public/{feeds[1]}/rotation-{rotations[1]}"
+                + ("-no-proof" if feeds[1] in NO_PROOF_SNAPSHOTS else "")
+                + ".json"
+            ),
+            feeds[2]: get_feed_data(
+                f"public/{feeds[2]}/rotation-{rotations[2]}"
+                + ("-no-proof" if feeds[2] in NO_PROOF_SNAPSHOTS else "")
+                + ".json"
+            ),
         },
         "postURLs": {
             feed_uuid: {
@@ -119,6 +139,7 @@ async def validate_participant(participant_id: str):
                     Params={
                         "Bucket": "trending-feeds",
                         "Key": f"public/{feed_uuid}/{uuid}"
+                        + ("-no-proof" if feed_uuid in NO_PROOF_SNAPSHOTS else "")
                         + (".jpg" if feed_uuid in JPG_SNAPSHOTS else ".png"),
                     },
                     ExpiresIn=3_600,
