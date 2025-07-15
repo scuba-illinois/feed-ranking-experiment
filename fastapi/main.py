2afa8ff9f03d815787c597c4bb4860c2100b0013
Jackie Chan <jackiec1998@gmail.com>
2025-07-15 16:46:39 -0400

    Serving new snapshots based on new meta.json schema to obfuscate treatments through file names on S3.

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 import pandas as pd
 import boto3
 import json
+import uuid
 from dotenv import load_dotenv
 import os
+import pprint
+import random
 from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv()
@@ -24,7 +28,7 @@ app.add_middleware(
 )
 
 
-def get_feed(key: str, bucket: str = "trending-feeds", expires_in: int = 3_600):
+def get_from_S3(key: str, bucket: str = "trending-feeds", expires_in: int = 3_600):
     return s3.generate_presigned_url(
         "get_object",
         Params={
@@ -35,14 +39,14 @@ def get_feed(key: str, bucket: str = "trending-feeds", expires_in: int = 3_600):
     )
 
 
-def get_feed_data(key: str, bucket: str = "trending-feeds"):
-    response = s3.get_object(Bucket=bucket, Key=key)
-    return json.load(response["Body"])
-
-
 def get_meta(uuid: str, bucket: str = "trending-feeds"):
     response = s3.get_object(Bucket=bucket, Key=f"public/{uuid}/meta.json")
     return json.load(response["Body"])
+
+
+def record_stimuli():
+
+    return
 
 
 @app.get("/")
@@ -59,93 +63,71 @@ async def validate_participant(participant_id: str):
     and the JSON associated with the rotation so that the buttons can be drawn.
     """
 
-    """
-    TODO: Eventually deprecate this when all snapshots are taken using JPGs.
-    """
-    JPG_SNAPSHOTS = {
-        "38dbd51f-fbff-4541-813b-80678f03ba43",
-        "5e24e85b-40db-4fa8-ae6e-cfe16cd6920e",
-    }
-    NO_PROOF_SNAPSHOTS = {"5e24e85b-40db-4fa8-ae6e-cfe16cd6920e"}
-
-    # Load the CSV file into a DataFrame
-    df = pd.read_csv("IDs.csv")
-
-    # TODO: Eventually what snapshots and rotations should depends on the participant ID.
-
-    # TODO: Eventually add back in whitelist check.
-    if participant_id not in df["ID"].values and False:
-        return {"valid": False}
-
-    # Read the meta.json for each feed UUID.
-    feeds = [
-        "5e24e85b-40db-4fa8-ae6e-cfe16cd6920e",  # No proof snapshot
-        "38dbd51f-fbff-4541-813b-80678f03ba43",  # New JPG snapshot
-        # "20e1d2ef-2268-4d4b-a724-5f5d61cfc896",
-        # "36a937d7-4fc4-4cbf-8705-83776c112078",
-        "5c18c574-32db-4028-b4ea-40e949ff81ba",
+    show_proof = random.choice([True, False])
+    rotations = [
+        random.randint(0, 9),
+        random.randint(0, 9),
+        random.randint(0, 9),
     ]
 
-    rotations = [0, 3, 4]
+    feeds = [
+        "5e95e7a5-6f6e-47a1-8677-8707e1b63e02",
+        "ebb00a74-0230-4380-ba31-d4c2593f212a",
+        "3992c1e1-17e6-4991-85fa-f9da14f30568",
+    ]
 
-    meta = {}
+    meta = {feed_uuid: get_meta(feed_uuid) for feed_uuid in feeds}
 
-    for feed_uuid in feeds:
-        meta[feed_uuid] = get_meta(feed_uuid)
+    """
+    TODO: Record the treatments given to the participant, store it in session-starts
+    cluster on MongoDB.
+    """
 
     return {
         "valid": True,
         "feeds": feeds,
         "feedURLs": [
-            # get_feed("public/20e1d2ef-2268-4d4b-a724-5f5d61cfc896/rotation-0.png"),
-            get_feed(
-                f"public/{feeds[0]}/rotation-{rotations[0]}"
-                + ("-no-proof" if feeds[0] in NO_PROOF_SNAPSHOTS else "")
-                + (".jpg" if feeds[0] in JPG_SNAPSHOTS else ".png")
+            get_from_S3(
+                f"public/{feeds[0]}/"
+                + meta[feeds[0]]["feeds"][str(rotations[0])][
+                    "proof" if show_proof else "noProof"
+                ]["feedUUID"]
+                + ".jpg"
             ),
-            get_feed(
-                f"public/{feeds[1]}/rotation-{rotations[1]}"
-                + ("-no-proof" if feeds[1] in NO_PROOF_SNAPSHOTS else "")
-                + (".jpg" if feeds[1] in JPG_SNAPSHOTS else ".png")
+            get_from_S3(
+                f"public/{feeds[1]}/"
+                + meta[feeds[1]]["feeds"][str(rotations[1])][
+                    "proof" if show_proof else "noProof"
+                ]["feedUUID"]
+                + ".jpg"
             ),
-            get_feed(
-                f"public/{feeds[2]}/rotation-{rotations[2]}"
-                + ("-no-proof" if feeds[2] in NO_PROOF_SNAPSHOTS else "")
-                + (".jpg" if feeds[2] in JPG_SNAPSHOTS else ".png")
+            get_from_S3(
+                f"public/{feeds[2]}/"
+                + meta[feeds[2]]["feeds"][str(rotations[2])][
+                    "proof" if show_proof else "noProof"
+                ]["feedUUID"]
+                + ".jpg"
             ),
         ],
         "feedData": {
-            feeds[0]: get_feed_data(
-                f"public/{feeds[0]}/rotation-{rotations[0]}"
-                + ("-no-proof" if feeds[0] in NO_PROOF_SNAPSHOTS else "")
-                + ".json"
-            ),
-            feeds[1]: get_feed_data(
-                f"public/{feeds[1]}/rotation-{rotations[1]}"
-                + ("-no-proof" if feeds[1] in NO_PROOF_SNAPSHOTS else "")
-                + ".json"
-            ),
-            feeds[2]: get_feed_data(
-                f"public/{feeds[2]}/rotation-{rotations[2]}"
-                + ("-no-proof" if feeds[2] in NO_PROOF_SNAPSHOTS else "")
-                + ".json"
-            ),
+            feeds[0]: meta[feeds[0]]["feeds"][str(rotations[0])][
+                "proof" if show_proof else "noProof"
+            ]["articleBounds"],
+            feeds[1]: meta[feeds[1]]["feeds"][str(rotations[1])][
+                "proof" if show_proof else "noProof"
+            ]["articleBounds"],
+            feeds[2]: meta[feeds[2]]["feeds"][str(rotations[2])][
+                "proof" if show_proof else "noProof"
+            ]["articleBounds"],
         },
         "postURLs": {
             feed_uuid: {
-                uuid: s3.generate_presigned_url(
-                    "get_object",
-                    Params={
-                        "Bucket": "trending-feeds",
-                        "Key": f"public/{feed_uuid}/{uuid}"
-                        + ("-no-proof" if feed_uuid in NO_PROOF_SNAPSHOTS else "")
-                        + (".jpg" if feed_uuid in JPG_SNAPSHOTS else ".png"),
-                    },
-                    ExpiresIn=3_600,
-                )
-                for _, uuid in meta[feed_uuid]["originalOrdering"].items()
+                post_uuid: get_from_S3(f"public/{feed_uuid}/{post_uuid}.jpg")
+                for post_uuid in meta[feeds[i]]["feeds"][str(rotations[i])][
+                    "proof" if show_proof else "noProof"
+                ]["posts"]
             }
-            for feed_uuid in feeds
+            for i, feed_uuid in enumerate(feeds)
         },
     }
 
